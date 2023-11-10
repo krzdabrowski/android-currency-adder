940c090dbbe97bc0469db81e1cc2e31ebe8d0693
Krzysztof Dabrowski <krz.dabrowski95@gmail.com>
2023-11-10 11:00:31 +0100

    Bump targetSdk to 34

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
     defaultConfig {
         applicationId = "eu.krzdabrowski.currencyadder"
         minSdk = 26
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 4
         versionName = "1.2.1"
     }
--- base-feature/build.gradle.kts
+++ base-feature/build.gradle.kts
@@ -15,7 +15,7 @@ android {
 
     with (defaultConfig) {
         minSdk = 26
-        targetSdk = 33
+        targetSdk = 34
     }
 
     defaultConfig {
