ef7d5e9b662ef73596f965851bdac2d3caba23c5
Krzysztof Dabrowski <krz.dabrowski95@gmail.com>
2024-07-13 15:34:38 +0200

    Bump version to 1.2.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "eu.krzdabrowski.currencyadder"
         minSdk = 26
         targetSdk = 34
-        versionCode = 7
-        versionName = "1.2.4"
+        versionCode = 8
+        versionName = "1.2.5"
     }
 
     buildFeatures {
